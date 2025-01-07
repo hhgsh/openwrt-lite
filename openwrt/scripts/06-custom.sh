1e5d43189e8fa5106185829d77729e862b3cbf4a
hhgsh <77732852+hhgsh@users.noreply.github.com>
2025-01-07 19:18:23 +0800

    Update 06-custom.sh

--- openwrt/scripts/06-custom.sh
+++ openwrt/scripts/06-custom.sh
@@ -6,7 +6,11 @@
 
 # alist - add new package
 git clone https://$github/sbwml/openwrt-alist package/new/alist
-
+git clone https://$github.com/sirpdboy/sirpdboy-package/tree/main/luci-app-advancedplus
+git clone https://$github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice
+git clone https://$github.com/kenzok8/openwrt-packages/tree/master/luci-app-adguardhome
+git clone https://$github.com/sirpdboy/sirpdboy-package/tree/main/luci-theme-kucat
+git clone https://$github.com/sirpdboy/sirpdboy-package/tree/main/luci-app-socat
 # lrzsz - add patched package
 rm -rf feeds/packages/utils/lrzsz
 git clone https://$github/sbwml/packages_utils_lrzsz package/new/lrzsz
