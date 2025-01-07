d1cd4aa8b1a425ee159aae234fd347bc0fae96d0
hhgsh <77732852+hhgsh@users.noreply.github.com>
2025-01-07 23:36:12 +0800

    Update 06-custom.sh

--- openwrt/scripts/06-custom.sh
+++ openwrt/scripts/06-custom.sh
@@ -5,11 +5,11 @@
 ### Then, add CONFIG_PACKAGE_luci-app-alist=y to the end of openwrt/23-config-common-custom
 
 # alist - add new package
-git clone https://$github/sbwml/openwrt-alist package/new/alist
-git clone https://$github.com/sirpdboy/sirpdboy-package/tree/main/luci-app-advancedplus
-git clone https://$github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice
-git clone https://$github.com/sirpdboy/sirpdboy-package/tree/main/luci-theme-kucat
-git clone https://$github.com/sirpdboy/sirpdboy-package/tree/main/luci-app-socat
+# git clone https://$github/sbwml/openwrt-alist package/new/alist
+git clone https://$github.com/kiddin9/kwrt-packages/tree/main/luci-app-advancedplus
+git clone https://$https://github.com/kiddin9/kwrt-packages/tree/main/luci-app-poweroffdevice
+git clone https://$github.com/kiddin9/kwrt-packages/tree/main/luci-theme-kucat
+git clone https://$github.com/kiddin9/kwrt-packages/tree/main/luci-app-socat
 # lrzsz - add patched package
 rm -rf feeds/packages/utils/lrzsz
 git clone https://$github/sbwml/packages_utils_lrzsz package/new/lrzsz
